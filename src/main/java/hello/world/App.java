c014d359045a7f22c8db14b5168926c5dd962a05
Gilles <gilles@zenaton.com>
2022-03-27 11:52:26 +0200

    bump version to 0.9.0

--- src/main/java/hello/world/App.java
+++ src/main/java/hello/world/App.java
@@ -1,10 +1,12 @@
 package hello.world;
 
 import io.infinitic.factory.InfiniticWorkerFactory;
-import io.infinitic.worker.InfiniticWorker;
+import io.infinitic.workers.InfiniticWorker;
+
+import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try(InfiniticWorker worker = InfiniticWorkerFactory.fromConfigFile("infinitic.yml")) {
             worker.start();
         }
--- src/main/java/hello/world/Client.java
+++ src/main/java/hello/world/Client.java
@@ -1,23 +1,32 @@
 package hello.world;
 
 import hello.world.workflows.HelloWorld;
-import io.infinitic.client.Deferred;
-import io.infinitic.client.InfiniticClient;
+import io.infinitic.common.clients.InfiniticClient;
 import io.infinitic.factory.InfiniticClientFactory;
 
+import java.io.IOException;
+
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try(InfiniticClient client = InfiniticClientFactory.fromConfigFile("infinitic.yml")) {
             // create a stub from HelloWorld interface
             HelloWorld helloWorld = client.newWorkflow(HelloWorld.class);
 
             int i = 0;
-            while (i < 100) {
+            while (i < 10) {
                 // asynchronous dispatch of a workflow
-                Deferred<String> deferred = client.dispatch(helloWorld::greet, String.valueOf(i));
+                String strI = String.valueOf(i);
+                client.dispatchAsync(helloWorld::greet, strI)
+                        .thenApply( (deferred) ->  {
+                            System.out.println("Workflow " + HelloWorld.class.getName() + " " + deferred.getId() + " (" + strI + ") dispatched!");
 
-                System.out.println("workflow " + HelloWorld.class.getName() + " " + deferred.getId() + " dispatched!");
+                            return null;
+                        })
+                        .exceptionally( (error) -> {
+                            System.err.println("Failed to dispatch (" + strI + "): "  + error.toString());
 
+                            return null;
+                        });
                 i++;
             }
         }
